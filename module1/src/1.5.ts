7980c86bcdcedcd79a9b5ddff6b710434b7a237c
Wasim Hossain <wasim.hossain003@gmail.com>
2025-04-30 20:35:49 +0600

    Update 1.5.ts

--- module1/src/1.5.ts
+++ module1/src/1.5.ts
@@ -1,9 +1,11 @@
 const user:{
+    genre: 'homo sapience'; // type--> literal types
     firstName:string;
     lastName?:string; //optional type
     age: number;
     isMarried: boolean
 } = {
+    genre:'kire'
     firstName:'wasim',
     lastName: "hossain",
     age:29,
