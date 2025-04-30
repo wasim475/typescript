6a65f32d5bac8537280728154a608e94d900a7da
Wasim Hossain <wasim.hossain003@gmail.com>
2025-04-30 19:49:36 +0600

    write about tuple and primitive and non primitive value in 1.4.ts

---
+++ module1/dist/index.js
@@ -0,0 +1,2 @@
+"use strict";
+const name = "wasim Hossain";
---
+++ module1/src/1.4.ts
@@ -0,0 +1,41 @@
+let friends: string[] = ["anik", "rony"];
+
+const roleNumber: number[] = [1, 3, 5];
+
+// tuple
+
+/**
+  টুপল (Tuple) TypeScript-এ টুপল হলো এমন একটি বিশেষ অ্যারে যেখানে প্রতিটি এলিমেন্টের টাইপ নির্দিষ্টভাবে নির্ধারিত থাকে 
+  এবং তাদের ক্রম (order) গুরুত্বপূর্ণ। এটি এমন ডেটা স্ট্রাকচার যেখানে নির্দিষ্ট সংখ্যক ও নির্দিষ্ট টাইপের ভ্যালু রাখা যায়,
+   যেমন: [string, number] মানে প্রথমটা string, পরেরটা number হতে হবে।
+
+✅ প্রিমিটিভ ভ্যালু (Primitive Values):
+string
+
+number
+
+boolean
+
+null
+
+undefined
+
+bigint
+
+symbol
+
+✅ নন-প্রিমিটিভ ভ্যালু (Non-Primitive Values):
+object
+
+array
+
+function
+
+class
+
+Map
+
+Set
+
+Date
+ */
