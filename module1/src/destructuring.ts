d38b60d6706ed8f17cb9f9be76fb283f1b31659d
Wasim Hossain <wasim.hossain003@gmail.com>
2025-05-01 07:59:40 +0600

    learn about destructuring

---
+++ module1/src/destructuring.ts
@@ -0,0 +1,11 @@
+const morePoorUser ={
+    name:{
+        firstName:'Wasim',
+        lastName:'Hossain'
+    },
+    contactNo: 1718885075,
+    nationality: "bangladeshi"
+
+}
+
+const {contactNo, name:{firstName}}= morePoorUser
