e651c2ca2767c6ce5483f2ecdce85f89bad02801
Wasim Hossain <wasim.hossain003@gmail.com>
2025-05-01 08:19:28 +0600

    learn about array destructuring in typescript

--- module1/src/destructuring.ts
+++ module1/src/destructuring.ts
@@ -8,4 +8,14 @@ const morePoorUser ={
 
 }
 
-const {contactNo, name:{firstName}}= morePoorUser
+const {contactNo, name:{firstName:firstPartOfName}}= morePoorUser // firstName এর পরিবর্তে firstPartOfName দিয়ে কল করতে পারবো
+
+// Array Destructuring
+
+const myFriends =['Rony','Mitu',"Shahin",'sohanur roni','anik','tushar']
+
+const [a,b,c,bestFriend]= myFriends // a = rony b= mitu c= shahin bestfriend = sohanur roni
+
+const [,,,againBestFriend]= myFriends // প্রথম তিনটা ভ্যালু স্কিপ করবে এবং  bestfriend = sohanur roni
+const [,,,againAndAgainBestFriend,...rest]= myFriends // প্রথম তিনটা ভ্যালু স্কিপ করবে এবং  bestfriend = sohanur roni থাকবে এবং anik tushar rest ভ্যারিয়েবল এ অ্যারে আকারে যুক্ত হবে
+
