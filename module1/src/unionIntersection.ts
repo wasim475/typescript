1a9862357ee642791e69dc51ba35084d191716ac
Wasim Hossain <wasim.hossain003@gmail.com>
2025-05-02 07:41:27 +0600

    learn about union

---
+++ module1/src/unionIntersection.ts
@@ -0,0 +1,7 @@
+type frontEndDeveloper = 'fakibuzz Developer'|'Junior Developer'
+
+type fullStactDeveloper = 'BackendDeveloper'|'Expart Developer'
+
+type developer = frontEndDeveloper|fullStactDeveloper
+
+const newDeveloper: developer = 'Junior Developer'
