3d7cb7fe26391a57872ae7d4ed0f98a6ac2822d8
Wasim Hossain <wasim.hossain003@gmail.com>
2025-05-02 19:33:45 +0600

    learn about unknown type

--- module1/src/nullableNeverUnknown.ts
+++ module1/src/nullableNeverUnknown.ts
@@ -9,3 +9,22 @@ function searchName(value:string | null) {
 }
 
 searchName("null")
+
+
+// Unknoen Type
+
+const getSpeedInMeterPerSecond = (value:unknown)=>{
+    if(typeof value ==='number'){
+        const convertedValue = (value*10000)/3600
+        console.log(`The speed is ${convertedValue.toFixed(1)} ms^-1`)
+    }
+    if(typeof value === 'string'){
+        const [valueInNumber, unit] = value.split(' ')
+        const convertedValue = (parseFloat(valueInNumber)*1000)/3600
+        console.log(`The speed is ${convertedValue.toFixed(1)} ms^-1`)
+    } else{
+        console.log("Wrong Input")
+    }
+}
+
+getSpeedInMeterPerSecond(undefined)
