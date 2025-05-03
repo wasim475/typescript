b10ec94152022fbc896ca85c2385ed1539d127b7
Wasim Hossain <wasim.hossain003@gmail.com>
2025-05-03 21:06:47 +0600

    Update introductionGenerics.ts

--- module2/src/introductionGenerics.ts
+++ module2/src/introductionGenerics.ts
@@ -1,12 +1,49 @@
-// generic type
-
-type GenericArray<T> = Array<T>;
-
-// const rollNumbers: number[] = [3, 6, 8];
-const rollNumbers: GenericArray<number> = [3, 6, 8];
-
-// const mentors: string[] = ["Mr. X", "Mr. Y", "Mr. Z"];
-const mentors: GenericArray<string> = ["Mr. X", "Mr. Y", "Mr. Z"];
-
-// const boolArray: boolean[] = [true, false, true];
-const boolArray: GenericArray<boolean> = [true, false, true];
+{
+    // generic type
+/* 
+    TypeScript-এর Generic Type হলো এমন একটি ফিচার, যেটা কোডকে বেশি flexible এবং reusable করে তোলে।
+    Generics তোমাকে এমন ফাংশন, ইন্টারফেস বা ক্লাস বানাতে সাহায্য করে যেটা যে কোনো টাইপের ডেটার সাথে কাজ করতে পারে,
+    কিন্তু সেই টাইপটা runtime-এ না বরং compile time-এ নির্ধারণ করা হয়... */
+  
+    type GenericArray<T> = Array<T>;
+  
+    // const rollNumbers: number[] = [3, 6, 8];
+    const rollNumbers: GenericArray<number> = [3, 6, 8];
+  
+    // const mentors: string[] = ["Mr. X", "Mr. Y", "Mr. Z"];
+    const mentors: GenericArray<string> = ["Mr. X", "Mr. Y", "Mr. Z"];
+  
+    // const boolArray: boolean[] = [true, false, true];
+    const boolArray: GenericArray<boolean> = [true, false, true];
+  
+    interface User {
+      name: string;
+      age: number;
+    }
+  
+    const user: GenericArray<User> = [
+      {
+        name: "Mezba",
+        age: 100,
+      },
+      {
+        name: "Jhankar Mahbub",
+        age: 110,
+      },
+    ];
+  
+    const add = (x: number, y: number) => x + y;
+  
+    add(30, 20);
+  
+    //generic tuple
+  
+    type GenericTuple<X, Y> = [X, Y];
+  
+    const manush: GenericTuple<string, string> = ["Mr. X", "Mr. Y"];
+  
+    const UserWithID: GenericTuple<number, { name: string; email: string }> = [
+      1234,
+      { name: "persian", email: "a@gmail.com" },
+    ];
+  }
