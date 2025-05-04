5f132c8e8947e94f7b7cb28974edcd5477f2b5d1
Wasim Hossain <wasim.hossain003@gmail.com>
2025-05-05 05:40:04 +0600

    learn about constraints

--- module2/src/functionWithGeneric.ts
+++ module2/src/functionWithGeneric.ts
@@ -21,6 +21,29 @@
      const resGeneric = createArrayWithGeneric<number>(1)
     //  const resObject = createArrayWithGeneric<{name:string, id:number}>({name:'Nadim Nafi', id:1234})
      const resObject = createArrayWithGeneric<user>({name:'Nadim Nafi', id:1234})
-     console.log(resObject)
+
+     const createArrayWithTuple = <T,Q>(param1:T, param2:Q):[T,Q]=>{
+        return [param1, param2]
+     }
+     const resTuple = createArrayWithTuple<string, number>('Bangladesh', 1971)
+    
+
+
+     const addCourseToStudent =<T>(student:T):T=>{
+        const cousrName = "NLW"
+        return {
+            ...student,cousrName
+        }
+     }
+
+     type Student ={
+        name:string,
+        id:number,
+        age:number,
+        isMarried: boolean
+     }
+
+     const resCourse = addCourseToStudent<Student>({name:'Nadim Nafi',id:1235, age:29, isMarried:false})
+     console.log(resCourse)
     //
 }
